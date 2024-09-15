f824d047188e5c47912a5c1f204a9e34118202fc
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2024-09-15 17:10:08 +0200

    07-arrays

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,12 @@
+let animales = ["chanchito", "caballo"];
+
+console.log(animales);
+console.log(animales[0]);
+animales[2] = 'dragón';
+console.log(animales);
+
+animales[10] = 'Pez';
+console.log(animales);
+
+console.log(typeof animales);
+console.log(animales.length);
