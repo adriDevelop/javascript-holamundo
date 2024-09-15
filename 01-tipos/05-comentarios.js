7f514766a82c9d677e6214a50e9607b42f8e8e20
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2024-09-15 16:54:10 +0200

    05-comentarios

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,14 @@
+// Esto es un comentario
+/*
+Este es un comentario multilinea
+Que puede ser de varias lineas
+Y aquí termina el comentario multilinea
+*/
+/**
+ * Este es un comentario 
+ * de muychas
+ * lineaas
+ * */
+let numero = 42; // comentario
+
+console.log(numero);
